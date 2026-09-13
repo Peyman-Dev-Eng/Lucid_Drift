fe2b461f70242c82205eba3350c5cc720e1ac92f
Develepment <2025julylyly@gmail.com>
2026-09-14 01:17:19 +0330

    before add read input event for windows os and now, this engine can read keyboard input event and handle this

--- main.cpp
+++ main.cpp
@@ -2,12 +2,6 @@
 #include <Vector.h>
 #include "Main/Engine/System/Input/input.h"
 
-auto SUM(auto a, auto b) {
-    return a + b;
-}
-
 int main() {
-    std::cout << typeid(SUM(1, 3.2)) == typeid(float) ? 1 : 0 << std::endl;
-    std::cout << SUM(10, 20) << std::endl;
     return 0;
 }
