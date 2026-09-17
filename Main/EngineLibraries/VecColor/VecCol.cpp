f0795e9aa852ebe43894e03064b2de56eda5578d
Develepment <2025julylyly@gmail.com>
2026-09-17 18:21:13 +0330

    feat(LDDrift): add VecColor library with color manipulation methods

--- Main/EngineLibraries/VecColor/VecCol.cpp
+++ Main/EngineLibraries/VecColor/VecCol.cpp
@@ -1,11 +1,71 @@
-//
-// Created by Develepment on 9/17/26.
-//
-
 #include "VecCol.h"
 
-namespace LDDrift
-{
-    namespace VecColor {
-    } // VecColor
-} // LDDrift
+LDDrift::VecCol::VecCol() = default;
+
+LDDrift::VecCol::VecCol(const float red, const float green, const float blue, const float alpha) :
+    R(red), G(green), B(blue), A(alpha) {}
+
+void LDDrift::VecCol::Red() {
+    R = 1.0f;
+    B = 0.0f;
+    G = 0.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Green() {
+    G = 1.0f;
+    B = 0.0f;
+    R = 0.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Blue() {
+    B = 1.0f;
+    G = 0.0f;
+    R = 0.0f;
+    A = 0.0f;
+}
+
+void LDDrift::VecCol::White() {
+    R = 1.0f;
+    G = 1.0f;
+    B = 1.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Black() {
+    R = 0.0f;
+    G = 0.0f;
+    B = 0.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Yellow() {
+    R = 1.0f;
+    G = 1.0f;
+    B = 0.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Orange() {
+    R = 1.0f;
+    G = 0.5f;
+    B = 0.0f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::Purple() {
+    R = 0.5f;
+    B = 0.0f;
+    G = 0.5f;
+    A = 1.0f;
+}
+
+void LDDrift::VecCol::SetColor(const float& red, const float& green, const float& blue, const float& alpha) {
+    R = red;
+    G = green;
+    B = blue;
+    A = alpha;
+}
+
+LDDrift::VecCol::~VecCol() = default;
--- Main/EngineLibraries/VecColor/VecCol.h
+++ Main/EngineLibraries/VecColor/VecCol.h
@@ -2,13 +2,26 @@
 #define LUCID_DRIFT_VECCOL_H
 
 
-namespace LDDrift::VecColor
+namespace LDDrift
 {
-    class VecCol
+    class VecCol final
     {
-    private:
+    public:
+        float R{}, G{}, B{}, A{};
+        VecCol();
+        VecCol(float red, float green, float blue, float alpha);
+        void Red();
+        void Green();
+        void Blue();
+        void Yellow();
+        void Orange();
+        void Purple();
+        void White();
+        void Black();
+        void SetColor(const float& red, const float& green, const float& blue, const float& alpha);
+        ~VecCol();
     };
 }
 
 
-#endif //LUCID_DRIFT_VECCOL_H
+#endif
