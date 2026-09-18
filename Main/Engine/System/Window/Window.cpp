0975f0d20e6a3452a05a780027c12af7007072a4
Develepment <2025julylyly@gmail.com>
2026-09-18 15:40:31 +0330

    refactor(Main/Engine/CMakeLists.txt, main.cpp, System/Window/Window.cpp, System/Window/Window.h): update engine structure and add window creation and rendering functionality

--- Main/Engine/System/Window/Window.cpp
+++ Main/Engine/System/Window/Window.cpp
@@ -1,8 +1,94 @@
-//
-// Created by Develepment on 9/18/26.
-//
-
 #include "Window.h"
 
-namespace LDDrift {
-} // LDDrift
+LDDrift::Window::Window() {
+    GLB_assert(glfwInit())
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    settings.title = "NONE";
+    settings.width = 500;
+    settings.height = 500;
+    settings.CanResize = false;
+    settings.FillColorScreen = {0.0f, 0.0f, 0.0f, 1.0f};
+}
+
+void LDDrift::Window::CreateWindow() {
+    screen = glfwCreateWindow(settings.width, settings.height, settings.title, nullptr, nullptr);
+    if (screen == nullptr) {
+        const char* description = nullptr;
+        const int code = glfwGetError(&description);
+
+        std::cerr << "GLFW window creation failed!\n";
+        std::cerr << "Error code: " << code << '\n';
+        std::cerr << "Description: "
+            << (description ? description : "Unknown")
+            << '\n';
+
+        return;
+    }
+    settings.IsOpen = true;
+    glfwMakeContextCurrent(screen);
+    GLB_assert(gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+}
+
+void LDDrift::Window::SetTitle(const char* title) {
+    glfwSetWindowTitle(screen, title);
+    settings.title = title;
+}
+
+void LDDrift::Window::SetSize(const int width, const int height) {
+    glfwSetWindowSize(screen, width, height);
+    settings.width = width;
+    settings.height = height;
+}
+
+void LDDrift::Window::SetFillScreenColor(const LDDrift::VecCol& color) {
+    settings.FillColorScreen = color;
+}
+
+void LDDrift::Window::ClearBuffer() const {
+    glfwMakeContextCurrent(screen);
+    glClearColor(settings.FillColorScreen.R, settings.FillColorScreen.G, settings.FillColorScreen.B,
+                 settings.FillColorScreen.A);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void LDDrift::Window::SetPosition(const int x, const int y) {
+    glfwSetWindowPos(screen, x, y);
+    settings.pos_x = x;
+    settings.pos_y = y;
+}
+
+void LDDrift::Window::DestroyWindow() const {
+    glfwDestroyWindow(screen);
+    glfwTerminate();
+}
+
+bool LDDrift::Window::ScreenIsOpen() const {
+    return settings.IsOpen;
+}
+
+void LDDrift::Window::Update() {
+    glfwMakeContextCurrent(screen);
+    glfwSwapBuffers(screen);
+    glfwPollEvents();
+    if (glfwWindowShouldClose(screen)) {
+        settings.IsOpen = false;
+    }
+}
+
+LDDrift::VecCol LDDrift::Window::GetFillScreenColor() const {
+    return settings.FillColorScreen;
+}
+
+LDDrift::VecPos2D LDDrift::Window::GetWindowSize() const {
+    return LDDrift::VecPos2D{static_cast<float>(settings.width), static_cast<float>(settings.height)};
+}
+
+LDDrift::VecPos2D LDDrift::Window::GetPosition() const {
+    return LDDrift::VecPos2D{static_cast<float>(settings.pos_x), static_cast<float>(settings.pos_y)};
+}
+
+LDDrift::Window::~Window() {
+    this->DestroyWindow();
+}
--- Main/Engine/System/Window/Window.h
+++ Main/Engine/System/Window/Window.h
@@ -1,9 +1,49 @@
 #ifndef LUCID_DRIFT_WINDOW_H
 #define LUCID_DRIFT_WINDOW_H
+#include <Assertions.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <VecCol.h>
+
+#include "VecPos.h"
 
 namespace LDDrift
 {
-    class Window {
+    class Window
+    {
+    private:
+        struct Settings
+        {
+            int pos_x, pos_y;
+            int width, height;
+            bool CanResize;
+            const char* title;
+            int FramerateLimit;
+            LDDrift::VecCol FillColorScreen;
+            bool IsOpen;
+        };
+
+    private:
+        GLFWwindow* screen{nullptr};
+        Settings settings;
+    private: // functions
+
+    public:
+        Window();
+        void CreateWindow();
+        void DestroyWindow() const;
+        void SetFillScreenColor(const LDDrift::VecCol& color);
+        [[nodiscard]] LDDrift::VecCol GetFillScreenColor() const;
+        void ClearBuffer() const;
+        void SetTitle(const char* title);
+        [[nodiscard]] const char* GetTitle() const;
+        void SetSize(int width, int height);
+        [[nodiscard]] VecPos2D GetWindowSize() const;
+        void SetPosition(int x, int y);
+        [[nodiscard]] VecPos2D GetPosition() const;
+        void Update();
+        [[nodiscard]] bool ScreenIsOpen() const;
+        ~Window();
     };
 }
 
