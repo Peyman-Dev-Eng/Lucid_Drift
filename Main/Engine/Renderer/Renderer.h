aa816590a12359497e0c98d745c381e0981cf05b
Develepment <2025julylyly@gmail.com>
2026-09-18 15:54:16 +0330

    feat(Engine): add renderer system and update main.cpp

---
+++ Main/Engine/Renderer/Renderer.h
@@ -0,0 +1,15 @@
+#ifndef LUCID_DRIFT_RENDERER_H
+#define LUCID_DRIFT_RENDERER_H
+
+namespace LDDrift
+{
+    class Renderer final
+    {
+    private:
+    public:
+        Renderer();
+        ~Renderer();
+    };
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,29 +1,5 @@
 #include <iostream>
 #include <thread>
-#include "Main/Engine/System/Input/input.h"
-#include "System/Window/Window.h"
-
-static const char* VertexShader = R"(
-#version 460 core
-layout (location = 0) in vec3 aPos;
-void main() {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}
-)";
-static const char* FragmentShader = R"(
-#version 460 core
-out vec4 fragColor;
-void main() {
-    fragColor = vec4(1.0,0.0,0.0,1.0);
-}
-)";
-
-static GLuint CompileShader(const GLenum type, const char* source) {
-    const GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, nullptr);
-    glCompileShader(shader);
-    return shader;
-}
 
 int main() {
 
